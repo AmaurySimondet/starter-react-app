9c85e9f3028a15ae3ad74b55dbd5981cf13e59e5
AmaurySimondet <amaury.simondet@hotmail.com>
2023-01-05 19:18:25 +0100

    setup done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { React } from "react";
+
+import Accueil from "./pages/Accueil";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// import eruda from "eruda";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    // //console
+    // let el = document.createElement('div');
+    // document.body.appendChild(el);
+
+    // eruda.init({
+    //     container: el,
+    //     tool: ['console', 'elements']
+    // });
+
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Accueil />}>
+                    {/* <Route path="blogs" element={<Blogs />} /> */}
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ src/pages/Accueil.jsx
@@ -0,0 +1,12 @@
+import { React, useState, useEffect } from 'react';
+
+function Accueil() {
+
+    return (
+        <div>
+            <h1>Bienvenue sur le site incroyable d´invitation au mariage de Floriane et Antoine</h1>
+        </div>
+    );
+}
+
+export default Accueil;
