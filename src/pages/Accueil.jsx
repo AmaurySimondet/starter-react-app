f8c839dc062f4140a732c72a7da1648c1b134a5c
Amaury <amaury.simondet@smood.ch>
2023-03-16 08:49:56 +0100

    starting first page

--- src/pages/Accueil.jsx
+++ src/pages/Accueil.jsx
@@ -4,8 +4,53 @@ function Accueil() {
 
     return (
         <div>
-            <h1>Le mariage de</h1>
-            <h1 className='dactyl'>Floriane et Antoine</h1>
+            <img className="background-image" src={require('../images/background-image.png')} />
+
+            <nav className="navbar navbar-expand navbar-light inscription-navbar">
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <a className="navbar-brand" href="/inscription">
+                    <img className="logo-navbar" alt="logo" src={require('../images/icons/logo.png')} />
+                </a>
+
+                <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+
+                    <ul className="navbar-nav mr-auto mt-2 mt-lg-0 navbar-elements basic-flex">
+                        <li className="nav-item active">
+                            <a className="navigbar-link text-navbar" href="/connexion">PROGRAMME</a>
+                        </li>
+                        <li className="nav-item active">
+                            <a className="navigbar-link text-navbar" href="/cgu">ACCES</a>
+                        </li>
+                        <li className="nav-item active">
+                            <a className="navigbar-link text-navbar" href="/cgu">HOTEL</a>
+                        </li>
+                        <li className="nav-item active">
+                            <a className="navigbar-link text-navbar" href="/cgu">RSVP</a>
+                        </li>
+                        <li className="nav-item active">
+                            <a className="navigbar-link text-navbar" href="/cgu">DRESS-CODE</a>
+                        </li>
+                        <li className="nav-item active">
+                            <a className="navigbar-link text-navbar" href="/cgu">INFO PRATIQUES</a>
+                        </li>
+                        <li className="nav-item active">
+                            <a className="navigbar-link text-navbar" href="/cgu">PHOTOS</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+
+
+            <div className='bacic-page'>
+
+                <div id="titles">
+                    <h1>Le mariage de</h1>
+                    <h1 className='dactyl'>Floriane et Antoine</h1>
+                </div>
+            </div>
+
         </div>
     );
 }
