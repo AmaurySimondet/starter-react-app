64f0df8f9a367d213142c61be12c81f7d8f94100
Amaury Simondet <amaury.simondet@AmaurySndetsMBP.lan>
2023-11-26 20:01:02 +0100

    redeploy

--- src/App.js
+++ src/App.js
@@ -3,18 +3,7 @@ import { React } from "react";
 import Accueil from "./pages/Accueil";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-// import eruda from "eruda";
-
 function App() {
-
-    // //console
-    // let el = document.createElement('div');
-    // document.body.appendChild(el);
-
-    // eruda.init({
-    //     container: el,
-    //     tool: ['console', 'elements']
-    // });
 
     return (
         <BrowserRouter>
